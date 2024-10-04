eed624f9e42102ade82514b4ce92b7ee11b8d5e5
Raven186 <samedov-sultan@mail.ru>
2024-10-04 23:20:30 +0300

    Lesson 3

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//Lesson_1.first();
+//Lesson_1.second();
+//Lesson_1.third();
+//Lesson_1.fourth();
+//Lesson_1.fifth();
+//Lesson_1.star();
+
+//Lesson_2.first();
+//Lesson_2.second();
+//Lesson_2.third();
+//Lesson_2.fourth();
+//Lesson_2.fifth();
+//Lesson_2.sixth();
+//Lesson_2.seventh();
+//Lesson_2.zvezda();
+    }
+}
